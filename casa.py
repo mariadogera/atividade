c0eee2d0390c5773b32545c0d5f8bab02427fd9a
mariadogera <miss5@aluno.ifal.edu.br>
2025-03-10 14:06:59 -0300

    Add files via upload

---
+++ casa.py
@@ -0,0 +1,5 @@
+# informar o nome do personagem de herry potter
+nome = input("qual seu nome?")
+
+# verificar qual a casa desse personagem 
+if nome == "harry potter" or nome == "hermione granger" or nome == "rony 
