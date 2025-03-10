2e22f028371ad790d5c0dcbe0c75a9095ea05b23
mariadogera <miss5@aluno.ifal.edu.br>
2025-03-10 14:13:28 -0300

    Add files via upload

---
+++ VeriWalker2.py
@@ -0,0 +1,8 @@
+print("ola ser humano!")
+x=int(input("min querer saber esse valor (x):"))
+y=int(input("min querer saber o valor desse tambem (y):"))
+
+if x  < y or x > y:
+    print("X é diferente Y")
+elif (x and y) == x or y:
+    print ("é tudo igualzinho ser humaninho")   
