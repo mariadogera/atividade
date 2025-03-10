da23588618e300216fc834c32b777bd833c98a20
mariadogera <miss5@aluno.ifal.edu.br>
2025-03-10 14:12:50 -0300

    Add files via upload

---
+++ school.py
@@ -0,0 +1,8 @@
+print("como poso te ajudar hoje?")
+#pergunta ao usuario qual a sua escola
+escola= input ("qual a sua escola:")
+#pergunte ao usuario qual o seu nome
+nome = input ("qual seu nome:")
+#texto completo
+print (f"ola, {nome}, do(a) {escola}. bem-vindo ao noso sistema")
+
